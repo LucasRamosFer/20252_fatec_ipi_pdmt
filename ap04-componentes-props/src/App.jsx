70ef03956a24fc41c415142b34a04445219ce223
Lucas <lucas.ramos46@fatec.sp.gov.br>
2025-09-11 23:33:55 -0300

    learn(ap04-react): encerra a apostila
    -agora completamos a aplicacao utilizando uma colecao de objetos JS

--- ap04-componentes-props/src/App.jsx
+++ ap04-componentes-props/src/App.jsx
@@ -2,6 +2,37 @@ import Pedido from "./Pedido"
 import Cartao from "./Cartao"
 import Feedback from "./Feedback"
 const App = () => {
+    const pedidos = [
+        {
+            data: '21/08/2025',
+            titulo: 'SSD',
+            icone: 'fa-hdd',
+            descricao: 'SSD Kingston'
+
+        },
+
+        {
+            data: '24/08/2025',
+            titulo: 'Concrete Maths',
+            icone: 'fa-livro',
+            descricao: 'Livro de Matemática'
+        },
+
+        {
+            data: '22/08/2025',
+            titulo: 'HEADPHONE',
+            icone: 'fa-headphones',
+            descricao: 'Headphone Bluetooth'
+        },
+
+        {
+            data: '23/08/2025',
+            titulo: 'Câmera',
+            icone: 'fa-camera',
+            descricao: 'Câmera GoPro'
+        }
+
+    ]
     const componenteFeedback = (
         <Feedback
             textOK="Já chegou"
@@ -9,61 +40,55 @@ const App = () => {
             funcaoOK={() => alert("Obrigado pelo feedback")}
             funcaoNOK={() => alert("Vamos verificar")}
         />
+    
+        
     )
+    const hippo = [
+        {
+            icone: 'fa-solid fa-hippo fa-3x'
+        },
+
+        {
+             icone:'fa-solid fa-hippo fa-flip-horizontal fa-2x'
+
+        },
+
+        {
+             icone:'fa-solid fa-hippo fa-md'
+        },
+
+        {
+            icone: 'fa-solid fa-hippo fa-flip-horizontal fa-md'
+        }
+
+    ]
+
+    
     return <div>
         <div>
-            <i class="fa-solid fa-hippo fa-3x"></i>
-            <i class="fa-solid fa-hippo fa-flip-horizontal fa-2x"></i>
-            <i class="fa-solid fa-hippo fa-md"></i>
-            <i class="fa-solid fa-hippo fa-flip-horizontal fa-md"></i>
-
+            {
+                hippo.map((hippos) => (
+                    <i class={hippos.icone}></i>
+                ))
+            }
+            
         </div>
         <div className="container border rounded my-2 p-3">
             <div className="row">
-                <div className="col-sm-12 col-lg-6 col-xxl-4">
-                    <Cartao cabecalho="22/08/2025">
-                        <Pedido
-                            icone="fa-hdd"
-                            titulo="SSD"
-                            descricao="SSD Kingston" />
-                            <div className="mt-3"></div>
-                            {componenteFeedback}
-                    </Cartao>
-
-                </div>
-                <div className="col-sm-12 col-lg-6 col-xxl-4">
-                    <Cartao cabecalho="22/08/2025">
-                        <Pedido
-                            icone="fa-book"
-                            titulo="Pequeno Príncipe"
-                            descricao="Classico livro" />
-                            <div className="mt-3"></div>
-                        {componenteFeedback}
-                    </Cartao>
-
-                </div>
-                <div className="col-sm-12 col-lg-6 col-xxl-4">
-                    <Cartao cabecalho="22/08/2025">
-                        <Pedido
-                            icone="fa-headphones"
-                            titulo="Headphone"
-                            descricao="Headphone Bluetooth" />
-                            <div className="mt-3"></div>
-                        {componenteFeedback}
-                    </Cartao>
-
-                </div>
-                <div className="col-sm-12 col-lg-6 col-xxl-4">
-                    <Cartao cabecalho="22/08/2025">
-                        <Pedido
-                            icone="fa-camera"
-                            titulo="Câmera"
-                            descricao="Câmera GoPro" />
-                            <div className="mt-3"></div>
-                        {componenteFeedback}
-                    </Cartao>
-
-                </div>
+                {
+                    pedidos.map((pedido) => (
+                        <div className="col-sm-12 col-lg-6 col-xxl-4">
+                            <Cartao cabecalho={pedido.data}>
+                                <Pedido
+                                    icone={pedido.icone}
+                                    titulo={pedido.titulo}
+                                    descricao={pedido.descricao} />
+                                <div className="mt-3"></div>
+                                {componenteFeedback}
+                            </Cartao>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     </div>
